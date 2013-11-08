7dff899530952d52a7f93693d9eba35c236187c5
Patrick Winkler <patrick.winkler@intergga.ch>
2013-11-08 17:21:09 +0100

    Added window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	sf::RenderWindow window( sf::VideoMode(800, 600), "Corpuscle" );
+	window.setVerticalSyncEnabled( true );
+
+	// main loop
+	while( window.isOpen() ){
+		// event loop
+		sf::Event event;
+		while( window.pollEvent( event ) ){
+			if( event.type == sf::Event::Closed ){
+				window.close();
+			}
+		}
+
+		window.clear();
+
+		window.display();
+	}
 }
