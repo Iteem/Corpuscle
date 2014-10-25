987c7f3e4634fac97e6c9ef18a25807eda316691
Patrick <patrick.winkler@intergga.ch>
2014-10-25 11:03:02 +0200

    Added SFGUI.

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <glm/glm.hpp>
+#include <SFGUI/SFGUI.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "renderer/rendermanager.hpp"
@@ -16,15 +17,39 @@ const sf::Vector2u   r16K( 15360,  8640 );
 
 const sf::Vector2u screenSize( r720p );
 const sf::Vector2u renderSize( r720p );
+
+const unsigned int guiSize = 80;
+
 const int sampleDisplayInterval = 10;
 const unsigned int numThreads = 4;
 
 int main()
 {
 	// Window.
-	sf::RenderWindow window( sf::VideoMode(screenSize.x, screenSize.y), "Corpuscle" );
+	sf::RenderWindow window( sf::VideoMode(screenSize.x, screenSize.y), "Corpuscle", sf::Style::Titlebar | sf::Style::Close  );
 	window.setVerticalSyncEnabled( true );
 
+	// GUI.
+	sfg::SFGUI sfgui;
+
+	auto sfgWindow = sfg::Window::Create( sfg::Window::Style::NO_STYLE );
+	sfgWindow->SetPosition( sf::Vector2f( 0.f, screenSize.y - guiSize ) );
+	sfgWindow->SetRequisition( sf::Vector2f( screenSize.x, guiSize ) );
+
+	auto box = sfg::Box::Create();
+	sfgWindow->Add( box );
+
+	auto alignment = sfg::Alignment::Create();
+	alignment->SetScale( sf::Vector2f( 0.f, 0.f ) );
+	alignment->SetAlignment( sf::Vector2f( 0.5f, 0.5f ) );
+	box->Pack( alignment );
+
+	auto button = sfg::Button::Create( "Close" );
+	button->GetSignal( sfg::Widget::OnLeftClick ).Connect( [&window]{ window.close(); } );
+	alignment->Add( button );
+
+	sfg::Desktop desktop;
+	desktop.Add( sfgWindow );
 
 	// Set up sprite to display.
 	sf::Image img;
@@ -38,6 +63,7 @@ int main()
 		static_cast<float>(screenSize.y)/static_cast<float>(renderSize.y)
 	);
 
+
 	// Set up RenderManager and start the rendering process.
 	RenderManager rm( glm::uvec2( renderSize.x, renderSize.y ), numThreads );
 	rm.loadSceneFromFile( "data/scene.json" );
@@ -46,13 +72,20 @@ int main()
 
 	unsigned int prevSamples = 0;
 
+	// Clock for measuring the frame time
+	sf::Clock frameClock;
+
 	// Main loop.
 	while( window.isOpen() ){
 		unsigned int samples = rm.getSamples();
 
+		float frameTime = frameClock.restart().asSeconds();
+
 		// Event loop.
 		sf::Event event;
 		while( window.pollEvent( event ) ){
+			desktop.HandleEvent( event );
+
 			if( event.type == sf::Event::Closed ){
 				window.close();
 			}
@@ -63,6 +96,8 @@ int main()
 				);
 			}
 		}
+
+		desktop.Update( frameTime );
 
 		if( prevSamples != samples ){
 			// Only update the image every Nth sample.
@@ -88,7 +123,10 @@ int main()
 
 		// Drawing.
 		window.clear();
+
 		window.draw( sprite );
+		sfgui.Display( window );
+
 		window.display();
 	}
 }
