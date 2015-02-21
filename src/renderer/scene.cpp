23a7abba3eb9a900b154221e62a9dbc286ff8fd4
Patrick <patrick.winkler@intergga.ch>
2015-02-21 17:47:57 +0100

    Visible chunks are now specified in the scene file.

--- src/renderer/scene.cpp
+++ src/renderer/scene.cpp
@@ -123,13 +123,28 @@ bool Scene::loadFromJSON( const std::string &path )
 	// Load NBT.
 	std::string nbtPath = pt.get( "nbt.path", "" );
 	if( nbtPath != "" ){
+		glm::ivec2 min = parseVector2( pt.get( "nbt.min", ""), glm::ivec2( 0, 0 ) );
+		glm::ivec2 max = parseVector2( pt.get( "nbt.max", ""), glm::ivec2( 0, 0 ) );
+
 		Map map;
-		if( map.load( nbtPath, glm::ivec2( -10, -8 ), glm::ivec2( 10, 2 ) ) ){
-			for( int x = -10*16; x < 11*16; ++x ){
+		if( map.load( nbtPath, min, max ) ){
+			for( int x = min.x * 16; x < ( max.x + 1 ) * 16; ++x ){
 				for( int y = 0; y < 256; ++y ){
-					for( int z = -8*16; z < 3*16; ++z ){
+					for( int z = min.y * 16; z < ( max.y + 1 ) * 16; ++z ){
 						if( map.isBlockVisible( glm::ivec3( x, y, z ) ) ){
-							m_objects.emplace_back( make_unique<Cuboid>( glm::vec3(x,y,z), glm::vec3( x + 1 , y + 1, z + 1), glm::vec3( 0.5f, 0.5f, 0.5f ) ) );
+							auto col = glm::vec3( 0.5f, 0.5f, 0.5f ) / 10.f;
+							switch( map.getBlock( glm::ivec3( x, y, z ) ).first )
+							{
+								case 1: col = glm::vec3( 0.56f, 0.56f, 0.56f ) / 10.f; break;
+								case 2: col = glm::vec3( 0.34f, 0.71f, 0.20f ) / 10.f; break;
+								case 3: col = glm::vec3( 0.52f, 0.37f, 0.26f ) / 10.f; break;
+								case 8: case 9: col = glm::vec3( 0.11f, 0.31f, 0.95f ) / 10.f; break;
+								case 17: case 162: col = glm::vec3( 0.31f, 0.24f, 0.15f ) / 10.f; break;
+								case 18: case 161: col = glm::vec3( 0.24f, 0.60f, 0.08f ) / 10.f; break;
+								case 99: col = glm::vec3( 0.71f, 0.11f, 0.11f ) / 10.f; break;
+								case 100: col = glm::vec3( 0.57f, 0.43f, 0.33f ) / 10.f; break;
+							}
+							m_objects.emplace_back( make_unique<Cuboid>( glm::vec3(x,y,z), glm::vec3( x + 1 , y + 1, z + 1), col ) );
 						}
 					}
 				}
