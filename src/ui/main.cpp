5e1b19335afcfd6e17ad1a14e2133c91559be513
Patrick Winkler <patrick.winkler@intergga.ch>
2017-01-29 19:00:41 +0100

    Switched from SFGUI to ImGui.

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
-#include <glm/glm.hpp>
-#include <SFGUI/SFGUI.hpp>
-#include <SFGUI/Widgets.hpp>
+#include <imgui.h>
+#include <imgui-sfml.h>
 #include <SFML/Graphics.hpp>
 
 #include "renderer/rendermanager.hpp"
 
 
 const unsigned int guiSize = 250;
-
-const int sampleDisplayInterval = 10;
 const unsigned int numThreads = 4;
 
 int main()
@@ -19,52 +16,19 @@ int main()
 	sf::Vector2u renderSize( windowSize );
 
 	// Window.
-	sf::RenderWindow window( sf::VideoMode(windowSize.x + guiSize, windowSize.y), "Corpuscle", sf::Style::Titlebar | sf::Style::Close );
+	sf::RenderWindow window( sf::VideoMode(windowSize.x, windowSize.y), "Corpuscle", sf::Style::Titlebar | sf::Style::Close );
 	window.setVerticalSyncEnabled( true );
 
 	// GUI.
-	sfg::SFGUI sfgui;
+	ImGui::SFML::Init(window);
 
-	auto box = sfg::Box::Create( sfg::Box::Orientation::VERTICAL, 5.f );
-
-	auto startButton = sfg::Button::Create( "Start" );
-	box->Pack( startButton );
-
-	auto pauseButton = sfg::Button::Create( "Pause" );
-	box->Pack( pauseButton );
-
-	auto entry = sfg::Entry::Create( "data/scene.json" );
-	box->Pack( entry );
-
-	auto loadButton = sfg::Button::Create( "Load" );
-	box->Pack( loadButton );
-
-	auto checkButton = sfg::CheckButton::Create( "Update preview" );
-	checkButton->SetActive( true );
-	box->Pack( checkButton );
-
-	auto spsLabel = sfg::Label::Create( "SPS: 0" );
-	box->Pack( spsLabel );
-
-	auto alignment = sfg::Alignment::Create();
-	alignment->SetScale( sf::Vector2f( 0.f, 0.f ) );
-	alignment->SetAlignment( sf::Vector2f( 0.5f, 0.5f ) );
-	alignment->Add( box );
-
-	auto sfgWindow = sfg::Window::Create( sfg::Window::Style::BACKGROUND );
-	sfgWindow->SetPosition( sf::Vector2f( windowSize.x, 0.f ) );
-	sfgWindow->SetRequisition( sf::Vector2f( guiSize, windowSize.y ) );
-	sfgWindow->Add( alignment );
-
-	sfg::Desktop desktop;
-	if(!desktop.LoadThemeFromFile("data/gui.theme"))
-		return 1;
-	desktop.Add( sfgWindow );
-	desktop.Refresh();
+	bool updatePreview = true;
+	std::string spsLabel;
+	std::array<char, 255> scenePath = {"data/scene.json"};
 
 	// Load scene.
 	Scene scene;
-	scene.loadFromJSON( "data/scene.json" );
+	scene.loadFromJSON( std::string( scenePath.data() ) );
 
 	// Set up RenderManager.
 	RenderManager rm( scene, numThreads );
@@ -78,57 +42,42 @@ int main()
 	tex.loadFromImage( img );
 	sf::Sprite sprite( tex );
 
-	startButton->GetSignal( sfg::Button::OnLeftClick ).Connect( std::bind( &RenderManager::startRendering, &rm ) );
-	pauseButton->GetSignal( sfg::Button::OnLeftClick ).Connect( std::bind( &RenderManager::stopRendering, &rm ) );
-
-	checkButton->GetSignal( sfg::CheckButton::OnStateChange ).Connect( [&]{ rm.setUpdateImage( checkButton->IsActive() ); } );
-
 	// Clock for measuring the frame time
 	sf::Clock frameClock;
 	sf::Clock renderPassClock;
 
 	unsigned int prevSamples = 0;
 
-	loadButton->GetSignal( sfg::Button::OnLeftClick ).Connect( [&]{
-		rm.stopRendering();
-
-		if( !scene.loadFromJSON( entry->GetText() ) ){
-			window.close();
-			return;
-		}
-		prevSamples = 0;
-		rm.reset();
-	} );
 
 	// Main loop.
 	while( window.isOpen() ){
-		float frameTime = frameClock.restart().asSeconds();
+		auto frameTime = frameClock.restart();
 
 		// Event loop.
 		sf::Event event;
 		while( window.pollEvent( event ) ){
-			desktop.HandleEvent( event );
+			ImGui::SFML::ProcessEvent(event);
 
 			if( event.type == sf::Event::Closed ){
 				window.close();
 			}
 			else if( event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::F2 ){
 				img.saveToFile(
-					"corpuscle-"+std::to_string(renderSize.x) + "x" + std::to_string(renderSize.y) +
+					"corpuscle-" + std::to_string(renderSize.x) + "x" + std::to_string(renderSize.y) +
 					"-" + std::to_string( rm.getSamples() ) + "SPP.png"
 				);
 			}
 		}
 
-		desktop.Update( frameTime );
+		ImGui::SFML::Update(window, frameTime);
 
 		unsigned int samples = rm.getSamples();
 
 		if( prevSamples != samples ){
-			int sps = std::floor( renderSize.x * renderSize.y / renderPassClock.restart().asSeconds() );
+			int sps = static_cast<int>( std::floor( renderSize.x * renderSize.y / renderPassClock.restart().asSeconds() ) );
 			prevSamples = samples;
 
-			if( checkButton->IsActive() ){
+			if( updatePreview ){
 				// Update Image.
 
 				img = rm.getImage();
@@ -146,7 +95,7 @@ int main()
 									std::floor( ( windowSize.y - sprite.getGlobalBounds().height ) / 2.f ) );
 			}
 
-			spsLabel->SetText( "SPS: " + std::to_string( sps ) );
+			spsLabel = "SPS: " + std::to_string( sps );
 
 			window.setTitle(
 				"Corpuscle @ "+std::to_string(renderSize.x) + "x" + std::to_string(renderSize.y) +
@@ -154,13 +103,47 @@ int main()
 			);
 		}
 
+		ImGui::SetNextWindowSize( {guiSize, window.getSize().y} );
+		ImGui::SetNextWindowPos( {window.getSize().x - guiSize, 0} );
+
+		ImGui::Begin("Window", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+
+		if (ImGui::Button("Start")) {
+			rm.startRendering();
+		}
+		if (ImGui::Button("Pause")) {
+			rm.stopRendering();
+		}
+
+		ImGui::InputText( "##scenePath", scenePath.data(), scenePath.size());
+		if (ImGui::Button("Load")) {
+			rm.stopRendering();
+
+			if( !scene.loadFromJSON( std::string( scenePath.data() ) ) ){
+				window.close();
+			}
+			prevSamples = 0;
+			rm.reset();
+		}
+
+		ImGui::Checkbox( "Update preview", &updatePreview );
+		rm.setUpdateImage( updatePreview );
+
+		ImGui::LabelText( "##sps", spsLabel.c_str() );
+
+		ImGui::End();
+
 
 		// Drawing.
+		window.resetGLStates(); // I'm not sure why this is needed.
 		window.clear();
 
 		window.draw( sprite );
-		sfgui.Display( window );
+		ImGui::Render();
 
 		window.display();
 	}
+
+	ImGui::SFML::Shutdown();
 }
